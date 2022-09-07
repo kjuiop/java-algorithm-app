d9e0026e2c8fc80c332b84eb6ea2f1af9d15bcc6
kjuiop <copying_y@naver.com>
2022-09-07 11:26:08 +0900

    backjoon Q10172

---
+++ src/main/java/com/gig/backjoon/Q10172.java
@@ -0,0 +1,21 @@
+package com.gig.backjoon;
+
+/**
+ * @author : JAKE
+ * @date : 2022/09/07
+ */
+public class Q10172 {
+
+    /**
+     * 아래 예제와 같이 개를 출력하시오.
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println(
+                "| \\_/ |\n" +
+                        "| q p |     /}\n(  0  ) \"\"\"\\ \n" +
+                "| \"^\"`      |\n" +
+                "| | _/=\\ \\__|");
+    }
+}
