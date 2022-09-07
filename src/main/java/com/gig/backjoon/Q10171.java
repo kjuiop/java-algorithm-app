c85e7726471c4205c6442a01b8e876ff32e0a59c
kjuiop <copying_y@naver.com>
2022-09-07 11:06:37 +0900

    init commit

---
+++ src/main/java/com/gig/backjoon/Q10171.java
@@ -0,0 +1,17 @@
+package com.gig.backjoon;
+
+/**
+ * @author : JAKE
+ * @date : 2022/09/07
+ */
+public class Q10171 {
+
+    /**
+     * 아래 예제와 같이 고양이를 출력하시오.
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
+    }
+}
