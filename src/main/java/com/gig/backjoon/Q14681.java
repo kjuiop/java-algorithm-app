bdf5f7b33fc6727c04c6f5c59e7599233bfef0f1
kjuiop <copying_y@naver.com>
2022-09-09 09:24:25 +0900

    Q14681

---
+++ src/main/java/com/gig/backjoon/Q14681.java
@@ -0,0 +1,31 @@
+package com.gig.backjoon;
+
+import java.util.Scanner;
+
+/**
+ * @author : JAKE
+ * @date : 2022/09/09
+ */
+public class Q14681 {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a >= 0 && b >= 0) {
+            System.out.println("1");
+        } else if (a >= 0 && b < 0) {
+            System.out.println("4");
+        } else if (a < 0 && b >= 0) {
+            System.out.println("2");
+        } else if (a < 0 && b < 0) {
+            System.out.println("3");
+        }
+
+    }
+}
